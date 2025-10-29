23d93a20a908a9784db6d2081316f71bdb0f344d
MFlisar <mflisar.development@gmail.com>
2025-10-29 14:11:23 +0100

    updated compose to 1.9.1 and material3 to 1.9.0

--- library/core/src/commonMain/kotlin/com/michaelflisar/composedebugdrawer/core/composables/sub/Spinner.kt
+++ library/core/src/commonMain/kotlin/com/michaelflisar/composedebugdrawer/core/composables/sub/Spinner.kt
@@ -7,9 +7,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -41,7 +41,7 @@ internal fun <T> Spinner(
             modifier = Modifier
                 .fillMaxWidth()
                 .focusable(false)
-                .menuAnchor(MenuAnchorType.PrimaryNotEditable),
+                .menuAnchor(ExposedDropdownMenuAnchorType.PrimaryNotEditable),
             readOnly = true,
             enabled = true,
             value = labelProvider(items[selected], selected),
